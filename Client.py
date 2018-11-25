3cd4960b612ee4db2f1987648387a9ee5877140d
khmlpy <43922054+khml@users.noreply.github.com>
2018-11-25 20:10:16 +0900

    change arg.

--- Client.py
+++ Client.py
@@ -5,9 +5,23 @@ from config import CredentialInfo
 from watson_machine_learning_client import WatsonMachineLearningAPIClient
 
 
-def get_client(credential=None):
-    if credential is None:
-        credential = CredentialInfo()
+def get_client(wml_credentials=None):
+    """
+    wml_credentials is dict that:
+        wml_credentials = {
+            "url": str,
+            "username": str,
+            "password": str,
+            "instance_id": str
+        }
 
-    client = WatsonMachineLearningAPIClient(credential.info)
+    if wml_credentials of arg is None, use default config info.
+
+    :param wml_credentials: CredentialInfo or dict
+    :return: WatsonMachineLearningAPIClient
+    """
+    if wml_credentials is None:
+        wml_credentials = CredentialInfo().info
+
+    client = WatsonMachineLearningAPIClient(wml_credentials)
     return client
